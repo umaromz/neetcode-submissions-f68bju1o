0ae03b4bbec5a0806cf64ac4d304042b83d2bd5e
umar <58665204+umaromz@users.noreply.github.com>
2026-04-12 13:54:29 +0100

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-3.py
@@ -0,0 +1,17 @@
+class Solution:
+    # Brute force solution
+    # def hasDuplicate(self, nums: List[int]) -> bool:
+    #     for i in range(len(nums)):
+    #         for j in range(i+1, len(nums)):
+    #             if nums[i] == nums[j]:
+    #                 return True
+    #     return False
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        nums.sort()
+        if len(nums) <= 1: 
+            return False
+        for i in range(len(nums)):
+                if nums[i] == nums[i - 1]:
+                    return True
+        return False
+        
